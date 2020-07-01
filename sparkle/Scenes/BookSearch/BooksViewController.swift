13f44169c77b263f6fcd05d34263bbe16ed8317d
ohayoukenchan <san.kun.sakana@gmail.com>
2020-07-01 18:55:23 +0900

    :construction: 画面側実装途中

--- sparkle/Scenes/BookSearch/BooksViewController.swift
+++ sparkle/Scenes/BookSearch/BooksViewController.swift
@@ -51,13 +51,13 @@ final class BooksViewController: UIViewController, HasWeakStateDisposeBag {
     }
 
     deinit {
+        reduxStore.dispatch(DisposeAction(disposeBag))
         deinitLog()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(collectionView)
-        print("\(self.state)DDDDDDDDDDDDDDDDDDDDDDDDDDDDD")
         collectionView.backgroundColor = .init(white: 0.9, alpha: 1.0)
         collectionView.refreshControl = refreshControl
 
@@ -69,11 +69,83 @@ final class BooksViewController: UIViewController, HasWeakStateDisposeBag {
         adapter.collectionView = collectionView
         adapter.rx.setDataSource(dataSource).disposed(by: disposeBag)
 
+        state
+            .bind(to: Binder(self) { $0.applyState($1) })
+            .disposed(by: disposeBag)
+        state
+            .map { $0.dataSourceElements }
+            .bind(to: adapter.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+        state
+            .map { $0.shouldShowRefreshing }
+            .bind(to: refreshControl.rx.isRefreshing)
+            .disposed(by: disposeBag)
+        state
+            .map { $0.shouldRequestTrigger }
+            .distinctUntilChanged()
+            .filterNil()
+            .bind(to: Binder(self) { $0.request($1.connectionType) })
+            .disposed(by: disposeBag)
+        state
+            .map { $0.shouldShowErrorNotificationTrigger }
+            .distinctUntilChanged()
+            .filterNil()
+            .bind(to: Binder(self) { $0.showErrorNotification($1) })
+            .disposed(by: disposeBag)
+
+        refreshControl.rx.controlEvent(.valueChanged)
+            .bind(to: Binder(self) { me, _ in me.refreshReload() })
+            .disposed(by: disposeBag)
+
+        networkErrorViewController.retryButton.rx.tap
+            .bind(to: Binder(self) { me, _ in me.request(.initialRequest) })
+            .disposed(by: disposeBag)
+
+        request(.initialRequest)
+        reduxStore.dispatch(FavoritesState.Action.loadFavoritesActionCreator())
+
         view.setNeedsUpdateConstraints()
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+    }
+
+    func request(_ connectionType: ConnectionType) {
+        reduxStore.dispatch(
+            ThisState.Action.requestAsyncCreator(
+                connectionType: connectionType,
+                disposeBag: disposeBag
+            )
+        )
+    }
+
+    func refreshReload() {
+        reduxStore.dispatch(
+            ThisState.Action.requestAsyncCreator(
+                connectionType: .refreshReload,
+                disposeBag: disposeBag
+            )
+        )
+    }
+
+    func applyState(_ state: ThisState) {
+        loadingViewController.view.isShown = state.shouldShowLoading
+        networkErrorViewController.view.isShown = state.shouldShowNetworkError
+        serverErrorViewController.view.isShown = state.shouldShowServerError
+        unknownErrorViewController.view.isShown = state.shouldShowUnknownError
+    }
+
+    func showErrorNotification(_ trigger: ErrorNotificationTrigger) {
+        errorNotificationViewController.show(with: trigger.message) // UIViewController+Ex
+    }
+
+    override func updateViewConstraints() {
+        if didSetupConstraints == false {
+            constraintToSafeAreaLayoutGuideAnchorHelper(from: collectionView, to: view)
+            didSetupConstraints = true
+        }
+        super.updateViewConstraints()
     }
 }
 
